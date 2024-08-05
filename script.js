0e3aa21acb393185984010e7c4448513e2de26e0
Alex  Ross <alexross@Alexs-Mac-mini.local>
2024-08-04 20:56:58 -0400

    Implemented highscore functionality.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector(".guess").value);
 */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -24,6 +25,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (score > 1 && guess > secretNumber) {
     document.querySelector(".message").textContent = "📈 Too High!";
     score--;
